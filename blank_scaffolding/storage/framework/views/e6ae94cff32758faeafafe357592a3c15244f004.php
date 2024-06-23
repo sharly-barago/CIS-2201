2d6c9eb92d997c7ee66a72492d58f9b643ff83ef
sharly-barago <155881364+sharly-barago@users.noreply.github.com>
2024-06-23 17:35:00 +0800

    Add files via upload

---
+++ blank_scaffolding/storage/framework/views/e6ae94cff32758faeafafe357592a3c15244f004.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" value="<?php echo e(csrf_token()); ?>"/>
+        <title>GESTOURAGE Inc.</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link href="<?php echo e(mix('css/app.css')); ?>" type="text/css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div id="gestourage">
+            <?php echo $__env->yieldContent('content'); ?>
+        </div>
+        <script src="<?php echo e(mix('js/app.js')); ?>" type="text/javascript"></script>
+    </body>
+</html><?php /**PATH C:\Users\Gestoni Cobar\gestourage\resources\views/layouts/app.blade.php ENDPATH**/ ?>
